3676ec4503295aa8fed553a66ab7ffb97207abf7
Andre Mendonca <andremendonca.033@gmail.com>
2023-05-06 14:54:42 +1000

    Footer Component

---
+++ components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { GlobalContext } from "@/GlobalContext";
+import styles from "@/styles/Footer.module.scss";
+
+const Footer = () => {
+  const global = React.useContext(GlobalContext);
+
+  return (
+    <footer className={styles.footerBg}>
+      <div className={styles.footer}>
+        <global.Link href="/" className={styles.footerLogo}>
+          <global.Image src="/logo-alt.png" alt="NeoPass Logo" width={197} height={42} />
+        </global.Link>
+
+        <nav className={styles.footerSocials}>
+          <p>Follow us on our socials:</p>
+          <div className={styles.socialsWrapper}>
+            <global.Link href="https://facebook.com">
+              <global.Image src="/facebook-icon.svg" alt="Facebook logo" width={20} height={20} />
+            </global.Link>
+            <global.Link href="https://instagram.com">
+              <global.Image src="/instagram-icon.svg" alt="Instagram logo" width={20} height={20} />
+            </global.Link>
+            <global.Link href="https://linkedin.com">
+              <global.Image src="/linkedin-icon.svg" alt="Linkedin logo" width={20} height={20} />
+            </global.Link>
+          </div>
+        </nav>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
